3fe06f8f8d3c73148387c99b22a67d531be4101c
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-02-12 13:48:16 -0800

    Lifted State Up

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -9,7 +9,7 @@ const NewExpense = (props) => {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+    props.onAddExpense(expenseData);
   };
 
   return (
